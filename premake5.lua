39e1893b8d3268d97aeb02bb210da5f4b60d8e1e
Menno <mennobil02@gmail.com>
2021-04-07 13:02:25 +0200

    [FEATURE] added premake

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "ByteCat"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
+project "ByteCat"
+	location "ByteCat"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BC_PLATFORM_WINDOWS",
+			"BC_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "BC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BC_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"ByteCat/vendor/spdlog/include",
+		"ByteCat/src"
+	}
+
+	links
+	{
+		"ByteCat"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BC_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "BC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BC_DIST"
+		optimize "On"
