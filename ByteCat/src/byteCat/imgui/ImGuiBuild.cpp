5af6abaa469252558dc9a512d26b3aaae8e5ea6c
Menno <mennobil02@gmail.com>
2022-04-10 11:50:36 +0200

    [ADDED] Winlin check to ImGuiBuild

--- ByteCat/src/byteCat/imgui/ImGuiBuild.cpp
+++ ByteCat/src/byteCat/imgui/ImGuiBuild.cpp
@@ -1,6 +1,6 @@
 #include "bcpch.h"
 
-#ifdef BC_PLATFORM_WINDOWS || BC_PLATFORM_LINUX
+#if defined(BC_PLATFORM_WINDOWS) || defined(BC_PLATFORM_LINUX)
 	#define IMGUI_IMPL_OPENGL_LOADER_GLAD
 	#include "examples/imgui_impl_opengl3.cpp"
 	#include "examples/imgui_impl_glfw.cpp"
