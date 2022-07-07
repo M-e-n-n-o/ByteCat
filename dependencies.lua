5fd7b73789a45c8f4d899c7810224631b7892d0b
Menno <mennobil02@gmail.com>
2022-07-07 14:06:15 +0200

    [REMOVED] premake5 files

--- dependencies.lua
+++
@@ -1,8 +0,0 @@
--- ByteCat dependencies
-
-Libs = {}
-Libs["GLFW"] = "../ByteCat/vendor/GLFW"
-Libs["GLAD"] = "../ByteCat/vendor/Glad"
-Libs["GLM"] = "../ByteCat/vendor/glm"
-Libs["ImGui"] = "../ByteCat/vendor/imgui"
-Libs["StbImage"] = "../ByteCat/vendor/stb_image"
